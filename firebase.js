663fe55b31bc2300ee84ac53ec7091de74e821c0
kingsley-business2 <kingsleybusiness2@gmail.com>
2025-09-21 16:53:06 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+// Your Firebase config (REPLACE WITH YOUR ACTUAL CONFIG)
+const firebaseConfig = {
+  apiKey: "AIzaSyBoKnl63xqbjv38DulILQsAGli9MZwrSKg",
+  authDomain: "my-agribusiness-2a808.firebaseapp.com",
+  projectId: "my-agribusiness-2a808",
+  storageBucket: "my-agribusiness-2a808.firebasestorage.app",
+  messagingSenderId: "982199975976",
+  appId: "1:982199975976:web:aee17e709b25986ebe351d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase initialized!");
+
+
+
+
+
