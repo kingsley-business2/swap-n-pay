949dbe7be209ccf42f3b0779eb940d82c10621ab
kingsley-business2 <kingsleybusiness2@gmail.com>
2025-09-21 17:36:03 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-// Service Worker
+// sw.js - Service Worker for PWA
+const CACHE_NAME = 'swap-n-stay-v1';
+const urlsToCache = ['/', '/index.html', '/admin.html'];
 
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installed");
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker activated");
-});
-
-self.addEventListener("fetch", (event) => {
-    console.log("Fetch intercepted:", event.request.url);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
